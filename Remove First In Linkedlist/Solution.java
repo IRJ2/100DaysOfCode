830df5125b27c7c82063db9d50582664e33344bb
Irene Rose Johnson <80707395+IRJ2@users.noreply.github.com>
2022-07-26 13:40:05 +0530

    Update Solution.java

--- Remove First In Linkedlist/Solution.java
+++ Remove First In Linkedlist/Solution.java
@@ -39,18 +39,15 @@ public class Main {
 
     public void removeFirst(){
       // write your code here
-      if(size==0){
-          System.out.print("List is empty");
+      if (size == 0) {
+        System.out.println("List is empty");
+      } else if (size == 1) {
+        head = tail = null;
+        size--;
+      } else {
+        head = head.next;
+        size--;
       }
-      else{
-          if(size==1){
-              head=tail=null;
-          }
-          else{
-              head=head.next;
-          }
-      }
-      size--;
     }
   }
 
